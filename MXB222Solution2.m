3b198fa82a9eb7b4e836da4ba5fa8517405e22a5
WashimNeupane <washim.neupane1@gmail.com>
2018-10-29 17:01:55 +1000

    Update
    Added iteration counts as output. 
    Fixed bug with chlsky("full", A).
    Solution to Ax = b using built in is included for diognising errors.

--- MXB222Solution2.m
+++ MXB222Solution2.m
@@ -4,28 +4,37 @@
 %%Check if coefficient matrix is SPD or not
 isSPD(A);
 
+%Take upper part of A
+% A = triu(A);
+
 %Generate the data structures
+full                         = triu(A);
 packed                       = GenerateDataStructures("packed", A);
 [banded, banded_RCM]         = GenerateDataStructures("band", A);
 [sprse , sparse_AMD]         = GenerateDataStructures("sparse", A);
 [CSR,CSR_RRow,CSR_RCol]      = GenerateDataStructures("CSR", A);
 
-%Generate cholesky factoristion of the data structures above. 
-full_chol                           = chlsky("full", A);
-[packed_chol,packed_chol_linear]    = chlsky("packed", packed);
+%%Generate cholesky factoristion of the data structures above. 
+full_chol                           = chlsky("full", full);
+
+%Note: packed_chol is full matrix, packed_chol_linear is packed matrix
+[packed_chol, packed_chol_linear]   = chlsky("packed", packed); 
 banded_chol                         = chlsky("band", banded);
 sprse_chol                          = chlsky("sparse", sprse);
 
 %Generate the solution to CSR using iterative methods
-CSR_Jacobi                          = IterativeMethodCSR("Jacobi", CSR,CSR_RRow,CSR_RCol,b);
-CSR_GaussSeidel                     = IterativeMethodCSR("GaussSiedel", CSR,CSR_RRow,CSR_RCol,b);
-CSR_ConjugateGradient               = IterativeMethodCSR("ConjugateGradient", CSR,CSR_RRow,CSR_RCol,b);
+[CSR_Jacobi, CSR_Jacobi_numIterations]                       = IterativeMethodCSR("Jacobi", CSR,CSR_RRow,CSR_RCol,b);
+[CSR_GaussSeidel, CSR_GaussSeidel_numIterations]             = IterativeMethodCSR("GaussSiedel", CSR,CSR_RRow,CSR_RCol,b);
+[CSR_ConjugateGradient, CSR_ConjugateGradient_numIterations] = IterativeMethodCSR("ConjugateGradient", CSR,CSR_RRow,CSR_RCol,b);
 
 %Generate SOR with varying omega values
-CSR_SOR1                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.01);
-CSR_SOR2                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.5);
-CSR_SOR3                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.94);
-CSR_SOR4                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.77);
+[CSR_SOR1,SOR1_numIterations]                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.01);
+[CSR_SOR2,SOR2_numIterations]                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.5);
+[CSR_SOR3,SOR3_numIterations]                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.94);
+[CSR_SOR4,SOR4_numIterations]                            = IterativeMethodCSR("SOR", CSR,CSR_RRow,CSR_RCol,b,0.77);
+
+%Solutions using direct methods
+Sol_full = full\b; 
 
 %Uncomment code below to generate all plots
 plots;
